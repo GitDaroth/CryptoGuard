103c601ecd751f14d752db155047c1e6ae745e57
GitDaroth <fabian.gulde@gmx.de>
2020-10-06 21:16:32 +0200

    integrated zxcvbn lib in order to estimate the strength of a password

--- CryptoGuard/include/Utils.h
+++ CryptoGuard/include/Utils.h
@@ -2,10 +2,11 @@
 
 #include <string>
 #include <iostream>
+#include <zxcvbn.h>
 
 namespace Utils
 {
-	inline std::string ulongToString(unsigned long ulong)
+	inline static std::string ulongToString(unsigned long ulong)
 	{
 		std::string outString;
 		outString += ulong >> 24;
@@ -15,21 +16,25 @@ namespace Utils
 		return outString;
 	}
 
-	inline void printHex(std::string message)
+	inline static void printHex(std::string message)
 	{
 		for (unsigned char c : message)
 		{
-			if (c / 16 < 10) std::cout << (char)((c / 16) + '0');
-			if (c / 16 >= 10) std::cout << (char)((c / 16 - 10) + 'a');
+			if (c / 16 < 10) 
+				std::cout << (char)((c / 16) + '0');
+			if (c / 16 >= 10) 
+				std::cout << (char)((c / 16 - 10) + 'a');
 
-			if (c % 16 < 10) std::cout << (char)((c % 16) + '0');
-			if (c % 16 >= 10) std::cout << (char)((c % 16 - 10) + 'a');
+			if (c % 16 < 10) 
+				std::cout << (char)((c % 16) + '0');
+			if (c % 16 >= 10) 
+				std::cout << (char)((c % 16 - 10) + 'a');
 
 			std::cout << " ";
 		}
 	}
 
-	inline std::string convertHexToString(std::string hexString)
+	inline static std::string convertHexToString(std::string hexString)
 	{
 		std::string outString;
 		for (int i = 0; i < hexString.size(); i += 2)
@@ -37,33 +42,27 @@ namespace Utils
 			int intFromHex;
 
 			if (hexString[i] >= '0' && hexString[i] <= '9')
-			{
 				intFromHex = (hexString[i] - '0') * 16;
-			}
 			else if (hexString[i] >= 'a' && hexString[i] <= 'z')
-			{
 				intFromHex = (hexString[i] - 'a' + 10) * 16;
-			}
 			else if (hexString[i] >= 'A' && hexString[i] <= 'Z')
-			{
 				intFromHex = (hexString[i] - 'A' + 10) * 16;
-			}
 
 			if (hexString[i + 1] >= '0' && hexString[i + 1] <= '9')
-			{
 				intFromHex += hexString[i + 1] - '0';
-			}
 			else if (hexString[i + 1] >= 'a' && hexString[i + 1] <= 'z')
-			{
 				intFromHex += hexString[i + 1] - 'a' + 10;
-			}
 			else if (hexString[i + 1] >= 'A' && hexString[i + 1] <= 'Z')
-			{
 				intFromHex += hexString[i + 1] - 'A' + 10;
-			}
 
 			outString += (char)intFromHex;
 		}
 		return outString;
+	}
+
+	inline static float calcPasswordStrength(const std::string& password)
+	{	
+		ZxcResult result = ZxcvbnMatch(password.c_str(), {}, nullptr);
+		return result.Score;
 	}
 }
